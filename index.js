7285c4313f7445b7a997c3f3efe12d40694af272
willian-hm <willianmoreira0510@gmail.com>
2026-02-04 17:31:30 -0300

    pra funcionar o dominio

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ app.get("/lista_usuarios", auth, apenasAdmin, (req, res) => {
 
 
 // Inicialização do servidor
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor rodando em http://0.0.0.0:${PORT}`);
 });
